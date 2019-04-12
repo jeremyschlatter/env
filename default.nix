88c99d59d8f5ab22f941426e572fb068aaf42036
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2019-04-12 11:19:06 -0700

    do not install nix if not needed (eg on NixOS)

--- default.nix
+++ default.nix
@@ -2,6 +2,16 @@ let
   pkgs = import (fetchTarball {
     url = https://github.com/NixOS/nixpkgs/tarball/19.03;
   }) {};
+
+  # This expression is designed to be installed with 'nix-env -ri', which deletes existing
+  # packages. If the nix tools are in the profile now, we want them to stay in the profile
+  # after 'nix-env -ri'.
+  #
+  # On NixOS, these tools are installed elsewhere. On other OS's, the default nix installation
+  # puts them in the profile.
+  maybe-nix = if builtins.pathExists ((builtins.getEnv "HOME") + "/.nix-profile/bin/nix-env")
+              then [pkgs.nix]
+              else [];
 in
 
 with pkgs; [
@@ -10,5 +20,4 @@ with pkgs; [
   tldr
   httpie
   tree
-  nix
-]
+] ++ maybe-nix
