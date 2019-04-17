605b2fc529f5dfce11eb3435f7d8bf9bca41c92f
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2019-04-17 14:22:25 -0700

    install more vim plugins

--- default.nix
+++ default.nix
@@ -37,12 +37,39 @@ let
       customRC = builtins.readFile ./vim-config.vim;
       packages.mine = with pkgs.vimPlugins; {
         start = [
+          ale
+          ctrlp-vim
+          vim-colors-solarized
+          vim-fetch
           vim-go
           vim-nix
+          vim-numbertoggle
           vim-unicoder
         ] ++ vim-plugins;
       };
      };
+  });
+
+  vim-fetch = (pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "vim-fetch";
+    version = "2019-04-03";
+    src = pkgs.fetchFromGitHub {
+      owner = "wsdjeg";
+      repo = "vim-fetch";
+      rev = "76c08586e15e42055c9c21321d9fca0677442ecc";
+      sha256 = "0avcqjcqvxgj00r477ps54rjrwvmk5ygqm3qrzghbj9m1gpyp2kz";
+    };
+  });
+
+  vim-numbertoggle = (pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "numbertoggle";
+    version = "2017-10-26";
+    src = pkgs.fetchFromGitHub {
+      owner = "jeffkreeftmeijer";
+      repo = "vim-numbertoggle";
+      rev = "cfaecb9e22b45373bb4940010ce63a89073f6d8b";
+      sha256 = "1rrmvv7ali50rpbih1s0fj00a3hjspwinx2y6nhwac7bjsnqqdwi";
+    };
   });
 
   vim-unicoder = (pkgs.vimUtils.buildVimPluginFrom2Nix {
