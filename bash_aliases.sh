jeremyschlatter/env

commit 5d024dd7f34854c6a01ebf3dac28c0cffbff2a00

bash: always use ls --color=auto
I don't remember why I disabled this on Linux in the first place.

Perhaps I'll find out.

bash_aliases.sh
@@ -1,8 +1,5 @@
-if test -n "$LINUX"; then
-    alias ls="ls -G --color=auto"
-else
-    alias ls="ls -G"
-fi
+alias ls="ls -G --color=auto"
+
 alias la="ls -A"
 alias sl="ls"
 alias l="ls"

commit e9c14bd7ce0bc62d1baf4421a242fddf1e21e4eb

fix ls on Darwin, broken in 5d024dd7f34854c6a0
Turns out I got the parity backward. This needs to be off in Darwin, not in
Linux.

bash_aliases.sh
@@ -1,4 +1,8 @@
-alias ls="ls -G --color=auto"
+if [ `uname` == "Darwin" ]; then
+    alias ls="ls -G"
+else
+    alias ls="ls -G --color=auto"
+fi
 
 alias la="ls -A"
 alias sl="ls"