0fe1234a8661d4f78f93018865badd4a762f6201
Jeremy Schlatter <jeremy@intelligence.org>
2019-10-08 14:12:37 -0700

    bash: delete obsolete aliases
    `n` was a tool I made and used for a bit, but haven't used in a long
    time.
    
    I don't use `ls` anymore -- I use `exa` instead.
    
    I don't use `grep` anymore -- I use `ripgrep` instead.

--- config/bash/aliases.sh
+++ config/bash/aliases.sh
@@ -2,7 +2,6 @@ alias ls="ls -G --color=auto"
 
 alias la="ls -A"
 alias sl="ls"
-alias l="ls"
 alias ll="ls -l"
 alias lrt="ls -lrt"
 
@@ -14,19 +13,11 @@ alias et="e --tree"
 
 alias "cd.."="cd .."
 alias python="python -tt"
-alias grep="grep --color"
 alias d=docker
 alias g=git
 alias gst="git status"
 alias gdiff="git diff"
 alias gadd="git add"
-
-alias nst="n status"
-alias ndiff="n diff"
-alias nadd="n add"
-alias ned="n edit"
-alias ncat="n cat"
-alias m="n message"
 
 alias gotop="gotop -c default-dark"
 
