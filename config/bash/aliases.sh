c30d3f3c27996e2b4f1aae66725540617e4a0460
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2020-05-14 20:10:09 -0700

    bash: add some more aliases

--- config/bash/aliases.sh
+++ config/bash/aliases.sh
@@ -11,13 +11,17 @@ alias ee="e --long --header --git"
 alias ert="e --long --sort time"
 alias et="e --tree"
 
-alias "cd.."="cd .."
-alias python="python -tt"
-alias d=docker
 alias g=git
 alias gst="git status"
 alias gdiff="git diff"
 alias gadd="git add"
+
+alias "cd.."="cd .."
+alias python="python -tt"
+alias d=docker
+alias c="gcloud compute"
+alias cs="gcloud compute instances"
+alias ha="hass-cli"
 
 alias gotop="gotop -c default-dark"
 
