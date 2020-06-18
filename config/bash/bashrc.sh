a690785f0f2a555d0ba0c85c3bd8221efe28c6bb
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2020-06-18 15:01:41 -0700

    use fzf for ctrl-r
    This used to work a long time ago. It may have broken when I switched to
    using nix. I switched laptops and operating systems at the same time, so
    it's very plausible that I would have not noticed this after spending
    many days or a few weeks slowly rebuilding my dev environment.
    
    This config broke because it used a file, ~/.fzf.bash, that I did not
    have checked into my dotfiles repo. Happily, it was easy to fix. The fzf
    derivation in nix includes a file that does the same thing. I just had
    to get the right path for it.
    
    fzf is a nice ctrl-r interface, and I've missed it. It's good to have it
    back.

--- config/bash/bashrc.sh
+++ config/bash/bashrc.sh
@@ -147,7 +147,7 @@ eval "$(restore_colors)"
 
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
 
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+. $NIX_PROFILE/share/fzf/key-bindings.bash
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 bind -x '"\C-p": vim $(fzf);'
