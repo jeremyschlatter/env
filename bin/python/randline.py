e9828303106cfb2f978d5d7e16028a6294aaf408
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2020-09-14 09:44:14 -0700

    add a little random-line utility from @fiddlemath

---
+++ bin/python/randline.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Usage: randline FILE [COUNT] [FILE2 [COUNT2] ...]
+
+Prints a random line from FILE.
+
+If a numeric COUNT is given, print COUNT random lines from FILE.
+If multiple FILEs are given, print random random line(s) from each.
+All lines in the output are whitespace-trimmed and space-separated.
+"""
+from sys import argv, exit
+from random import choice
+
+try:
+    arr = []
+    i = 1
+    while i < len(argv):
+        filename = argv[i]
+        if i+1 < len(argv) and argv[i+1].isnumeric():
+            count = int(argv[i+1])
+            i += 2
+        else:
+            count = 1
+            i += 1
+        with open(filename) as f:
+            lines = f.readlines()
+            for _ in range(count):
+                arr.append(choice(lines).strip())
+
+    print(" ".join(arr))
+except:
+    print(__doc__)
+    print()
+
