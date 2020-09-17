6718ba2411785948d0ff4117801e6e82730e8572
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2020-09-17 11:15:51 -0700

    bash: always use globstar

--- config/bash/bashrc.sh
+++ config/bash/bashrc.sh
@@ -8,6 +8,9 @@ HISTCONTROL=ignoredups
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
 
+# allow recursive ** globs
+shopt -s globstar
+
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
