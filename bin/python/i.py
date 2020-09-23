59410f148b6047a49f8e56a207eba7cbd4e37012
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2020-09-23 13:54:32 -0700

    port 'i' script to python

---
+++ bin/python/i.py
@@ -0,0 +1,30 @@
+import itertools
+import json
+import os
+import subprocess
+
+profile = os.path.expanduser('~/.flake')
+
+with open(f'{profile}/drvpath') as f:
+    drvpath = f.read().strip()
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+inputs = flatten(zip(
+    itertools.repeat('--update-input'),
+    json.loads(
+        subprocess.check_output(['nix', 'flake', 'list-inputs', '--json', drvpath])
+    )['nodes']['root']['inputs'].keys(),
+))
+
+subprocess.check_call(
+    ['nix', 'build']
+      + inputs
+      + [
+          '--no-link',
+          '--profile', f'{profile}/profile',
+          drvpath
+        ],
+)
+subprocess.check_call(['jeremy-post-install'])
