7ee6ef2c472b9e58d24ac1362d5db811ccd0f6fe
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2020-10-04 11:25:54 -0700

    pydark,pylight: better behavior over ssh
    Also rearrange this code now that a clear structure has been revealed.

--- bin/python/base.py
+++ bin/python/base.py
@@ -29,33 +29,48 @@ def base():
 color_config_path = os.path.expanduser('~/.config/colors')
 
 def set_colors(which, restore=False):
-    # set terminal colors (unless we're in a neovim terminal)
-    if not os.getenv('VIMRUNTIME'):
-        # Which terminal emulator are we in?
-        if 'kitty' in os.getenv('TERM'):
-            # kitty
-            run(['kitty', '@', 'set-colors', '--configured', '--all', f'~/.flake/profile/config/kitty/{which}.conf', ], check=True)
-        else:
-            # Assume iterm2.
-            #
-            # We don't need to restore on iterm2, because the color is stored in the profile.
-            # And every time we change the color, iterm2 pops up a confirmation box, which
-            # we don't want on every new tab and window.
-            if not restore:
-                async def iterm2_set_colors(connection):
-                    preset = await iterm2.ColorPreset.async_get(
-                        connection,
-                        f'Solarized {"Dark" if which == "dark" else "Light"}',
-                    )
-                    for partial in (await iterm2.PartialProfile.async_query(connection)):
-                        if partial.name in ["Default"]:
-                            await partial.async_set_color_preset(preset)
-                iterm2.run_until_complete(iterm2_set_colors)
+    # Set terminal colors.
+    #
+    # There are several possible terminals I might be in, and the behavior I
+    # want is different for each one.
+    if os.getenv('VIMRUNTIME'):
+        # neovim terminal
+        #
+        # Do nothing to the terminal, but continue with other changes.
+        pass
+    elif 'kitty' in os.getenv('TERM'):
+        # kitty
+        run(['kitty', '@', 'set-colors', '--configured', '--all', f'~/.flake/profile/config/kitty/{which}.conf', ], check=True)
+    elif os.getenv('TERM_PROGRAM') == 'iTerm.app':
+        # iterm2
+        #
+        # We don't need to restore on iterm2, because the color is stored in the profile.
+        # And every time we change the color, iterm2 pops up a confirmation box, which
+        # we don't want on every new tab and window.
+        if not restore:
+            async def iterm2_set_colors(connection):
+                preset = await iterm2.ColorPreset.async_get(
+                    connection,
+                    f'Solarized {"Dark" if which == "dark" else "Light"}',
+                )
+                for partial in (await iterm2.PartialProfile.async_query(connection)):
+                    if partial.name in ["Default"]:
+                        await partial.async_set_color_preset(preset)
+            iterm2.run_until_complete(iterm2_set_colors)
+    elif os.getenv('SSH_TTY'):
+        # Connected over ssh.
+        #
+        # The terminal is therefore not running on this machine, and we
+        # will not try to manipulate its colors.
+        #
+        # Do nothing to the terminal, but continue with other changes.
+        pass
 
-    # bat config var
+    # Set bat config var.
     print(f'export BAT_THEME="Solarized ({which})"')
 
-    # persist for next time
+    # Persist for next time.
+    # My vim config also reads this file to determine colors on startup.
     with open(color_config_path, 'w') as f:
         f.write(which)
 
