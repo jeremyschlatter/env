4953135c1fa0b99d8727e18d733c106370280649
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2021-06-19 13:41:03 -0700

    i.py: never write lock file
    This is so I don't constantly see the "warning: x is dirty" messages
    when re-installing my user environment.

--- scripts/i.py
+++ scripts/i.py
@@ -21,9 +21,8 @@ inputs = flatten(zip(
 subprocess.check_call(
     ['nix', 'build']
       + inputs
-      # Don't try to write the lockfile if this is a remote derivation.
-      + (['--no-write-lock-file'] if ':' in target['flake'] else [])
       + [
+          '--no-write-lock-file',
           '--no-link',
           '--profile', target['profile'],
           target['flake'],
