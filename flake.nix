aff14069d8be42d3f3cd125ba839285dcf290275
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2021-07-14 22:23:36 -0700

    -cargo

--- flake.nix
+++ flake.nix
@@ -85,7 +85,6 @@
         cachix           # User-managed binary caches for nix.
         caddy            # Run a webserver.
         calc             # A simple calculator.
-        cargo            # Build Rust code.
         cloc             # Count lines of code.
         coreutils  # Basic file, shell and text manipulation utilities.
         delta      # Better git diffs.
