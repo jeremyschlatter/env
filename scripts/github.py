89027dcf4ac9a5d4fa3876ccd3c7d8ee083d70c4
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2022-04-12 22:26:10 -0700

    scripts/github: drop hub dependency

--- scripts/github.py
+++ scripts/github.py
@@ -1,4 +1,4 @@
-# {"deps": ["gitAndTools.hub"]} #nix
+# {"deps": ["git"]} #nix
 from pathlib import Path
 from subprocess import run
 import sys
@@ -11,6 +11,6 @@ repo = sys.argv[1]
 dest = Path.home() / 'src' / 'github.com' / Path(repo)
 if not dest.is_dir():
     dest.parent.mkdir(parents=True, exist_ok=True)
-    if run(['hub', 'clone', repo, dest]).returncode:
+    if run(['git', 'clone', f'git@github.com:{repo}.git', dest]).returncode:
         sys.exit(1)
 print('cd', dest)
