15d966c2ad944cde2fce7da5a3f34adb5c6038e1
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2022-06-09 05:55:40 -0700

    line things up nicely

--- flake.nix
+++ flake.nix
@@ -57,58 +57,60 @@
     # Note that I am not limited to adding packages. I can delete or change anything here and
     # it will effectively delete or change the software on all of my machines.
     profile = { pkgs, unstable, system, super, x86 }:
+      with pkgs;
       let
-        my-configs = self.copyDir pkgs "my-configs" ./config "$out/config";
-        my-shell = pkgs.writeShellScriptBin "shell" ''exec ${pkgs.bashInteractive_5}/bin/bash --rcfile ${./config/bash/bashrc.sh} "$@"'';
-        my-vim = import ./neovim.nix pkgs;
-        themed = light: dark: pkg: pkgs.writeShellScriptBin pkg.pname ''
-          [[ $(${pkgs.coreutils}/bin/cat ~/.config/colors) = 'light' ]] && v='${light}' || v='${dark}'
+        configs = self.copyDir pkgs "my-configs" ./config "$out/config";
+        shell = writeShellScriptBin "shell" ''exec ${bashInteractive_5}/bin/bash --rcfile ${./config/bash/bashrc.sh} "$@"'';
+        vim = import ./neovim.nix pkgs;
+        themed = light: dark: pkg: writeShellScriptBin pkg.pname ''
+          [[ $(${coreutils}/bin/cat ~/.config/colors) = 'light' ]] && v='${light}' || v='${dark}'
           env "$v" ${pkg}/bin/${pkg.pname} $@
         '';
         bat-themed = themed "BAT_THEME=Solarized (light)" "BAT_THEME=Solarized (dark)";
-        fixGL = pkg: [pkg (pkgs.hiPrio (pkgs.writeShellScriptBin pkg.pname ''
+        fixGL = pkg: [pkg (hiPrio (writeShellScriptBin pkg.pname ''
           ${nixGL.packages."${system}".nixGLIntel}/bin/nixGLIntel ${pkg}/bin/${pkg.pname} $@
         ''))];
+        mcfly = (themed "MCFLY_LIGHT=1" "=" pkgs.mcfly);
       in
 
-      with pkgs; super ++ [
-        my-configs # Config files for some of the programs in this list.
+      super ++ [
+        configs # Config files for some of the programs in this list.
         (self.scripts system pkgs ./scripts) # Little utility programs.
 
         # My shell.
-        my-shell
+        shell
 
         # Undollar: ignore leading $'s from copy-pasted commands.
         (writeShellScriptBin "$" "\"$@\"")
 
         # Life on the command line.
-        (bat-themed bat)     # Display files, with syntax highlighting.
-        bash-completion  # Tab-completion for a bunch of commands.
-        caddy            # Run a webserver.
-        comma            # Use programs from the nix repo without installing them.
-        coreutils        # Basic file, shell and text manipulation utilities.
-        (bat-themed delta)   # Better git diffs.
-        direnv     # Set environment variables per-project.
-        docker     # Bundle programs with their dependencies.
-        exa        # List files in the current directory.
-        fd         # Find file by name.
-        fira-code  # Font that renders symbols in code nicely.
-        fzf        # Fuzzy text search.
-        git        # Track version history for text files.
-        gitAndTools.hub   # GitHub CLI.
-        gnumake           # Near-omnipresent generic build tool.
-        go_1_18           # Run Go code.
-        google-cloud-sdk  # Google Cloud CLI.
-        gotools           # Tools to facilitate coding in Go.
-        htop              # Show CPU + memory usage.
-        x86.httpie        # Create and execute HTTP queries.
+        (bat-themed bat)      # Display files, with syntax highlighting.
+        bash-completion       # Tab-completion for a bunch of commands.
+        caddy                 # Run a webserver.
+        comma                 # Use programs from the nix repo without installing them.
+        coreutils             # Basic file, shell and text manipulation utilities.
+        (bat-themed delta)    # Better git diffs.
+        direnv                # Set environment variables per-project.
+        docker                # Bundle programs with their dependencies.
+        exa                   # List files in the current directory.
+        fd                    # Find file by name.
+        fira-code             # Font that renders symbols in code nicely.
+        fzf                   # Fuzzy text search.
+        git                   # Track version history for text files.
+        gitAndTools.hub       # GitHub CLI.
+        gnumake               # Near-omnipresent generic build tool.
+        go_1_18               # Run Go code.
+        google-cloud-sdk      # Google Cloud CLI.
+        gotools               # Tools to facilitate coding in Go.
+        htop                  # Show CPU + memory usage.
+        x86.httpie            # Create and execute HTTP queries.
         less                  # Scroll through long files.
-        (python3.withPackages (pkgs: with pkgs; [
-          ipython             # Better Python repl than the default.
-        ]))                   # Run Python.
+        (python3.withPackages # Run Python.
+          (ps: [ps.ipython])) # Better Python repl than the default.
         x86.magic-wormhole    # Copy files between computers.
         man-db                # View manuals. (Present on most OS's already -- this just ensures a recent version).
-        my-vim                # Edit text.
+        mcfly                 # Shell history search.
+        vim                   # Edit text.
         nix-direnv            # Optimized direnv+nix integration.
         nix-index             # Find which nix package has the program you need.
         ripgrep               # Text search. (Phenomenal grep replacement.)
@@ -116,14 +118,12 @@
         unzip                 # Open .zip files.
         watch                 # Run a command repeatedly.
         wget                  # Download files.
-        zoxide
-        (themed "MCFLY_LIGHT=1" "=" mcfly)
+        zoxide                # A smarter cd command.
       ] ++ lib.optionals (system == "x86_64-linux") [
-        # My terminal. On macOS I use iTerm2 instead of kitty.
-        (fixGL kitty)
-        file   # Get high-level semantic info about a file.
-        nix
-        # etcher # Burn .iso images to USB drives and SD cards, w/ user-friendly GUI.
+        file                  # Get high-level semantic info about a file.
+        (fixGL kitty)         # My terminal. On macOS I use iTerm2 instead of kitty.
+        nix                   # Nix.
+        # etcher                # Burn .iso images to USB drives and SD cards, w/ user-friendly GUI.
       ];
   };
 }
