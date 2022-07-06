869c7a25a2541cd912a11b4b63e0b717f4850adc
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2022-07-06 08:57:02 -0700

    i.py: `--recreate-lock-file` -> `nix flake update`
    The former seems to have stopped having the behavior I want?

--- scripts/i.py
+++ scripts/i.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import sys
 
+from pathlib import Path
+
 targets = list(filter(lambda s: s.endswith('-bundled-environment'),
     subprocess.check_output(['nix', 'profile', 'list']).decode().strip().split('\n')))
 
@@ -17,16 +19,17 @@ flake  = targets[0].split()[1].split('#')[0]
 def flatten(l):
     return [item for sublist in l for item in sublist]
 
+inputs = flatten(zip(
+    itertools.repeat('--update-input'),
+    json.loads(
+        subprocess.check_output([
+            'nix', 'flake', 'metadata', '--json', flake,
+    ]))['locks']['nodes']['root']['inputs'].keys(),
+))
+
 if os.getenv('I_DOT_PY_DO_FULL_UPDATE'):
-    inputs = ['--recreate-lock-file']
-else:
-    inputs = flatten(zip(
-        itertools.repeat('--update-input'),
-        json.loads(
-            subprocess.check_output([
-                'nix', 'flake', 'metadata', '--json', flake,
-        ]))['locks']['nodes']['root']['inputs'].keys(),
-    ))
+    subprocess.check_call(['nix', 'flake', 'update'], cwd=(Path.home() / 'nix' / 'public-base'))
 
 subprocess.check_call(['nix', 'profile', 'upgrade'] + inputs + [index])
+
 subprocess.check_call(['jeremy-post-install'])
