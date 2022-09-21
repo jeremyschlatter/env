a676c0bd24aa229774e8ce9579bf9114cbf147c5
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2022-09-20 22:13:26 -0700

    compress bashrc

--- config/bash/bashrc.sh
+++ config/bash/bashrc.sh
@@ -1,27 +1,14 @@
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-shopt -s checkwinsize
-
-# allow recursive ** globs
-shopt -s globstar
+shopt -s checkwinsize  # update LINES and COLUMNS after each command
+shopt -s globstar      # allow recursive ** globs
+set   -o vi            # use vi-mode editing on the command line
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
 export NIX_PROFILE=$HOME/.nix-profile
-
-# Some aliases
-. $NIX_PROFILE/config/bash/aliases.sh
-
-# Set env vars
-. $NIX_PROFILE/config/bash/env.sh
-
-set -o vi  # Use vi-mode editing on the command line.
-
-source $NIX_PROFILE/etc/profile.d/bash_completion.sh
 
 # source bash completions
 if [ -d $NIX_PROFILE/etc/bash_completion.d ]; then
@@ -30,13 +17,15 @@ if [ -d $NIX_PROFILE/etc/bash_completion.d ]; then
     done
 fi
 
+. $NIX_PROFILE/config/bash/aliases.sh   # Some aliases
+. $NIX_PROFILE/config/bash/env.sh       # Set env vars
+. $NIX_PROFILE/etc/profile.d/bash_completion.sh
+. $NIX_PROFILE/share/bash-completion/completions/git
+
 # git tab completion with 'g' alias
-source $NIX_PROFILE/share/bash-completion/completions/git
 __git_complete g __git_main
 
-# Set color-related env variables according to the current color scheme
 eval "$(colorscheme restore-colors)"
-
 eval "$(direnv hook bash)"
 eval "$(mcfly init bash)"
 eval "$(starship init bash)"
