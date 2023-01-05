6c1616bdad271d030d2c85935a0bdb8f054d2e08
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2023-01-05 00:58:23 -0800

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 set -eux
 curl -L https://nixos.org/nix/install | sh
 . $HOME/.nix-profile/etc/profile.d/nix.sh
-nix-env -iAf https://github.com/NixOS/nixpkgs/tarball/release-21.05 nixUnstable
 sudo mkdir -p /etc/nix && echo 'experimental-features = nix-command flakes ca-references' | sudo tee -a /etc/nix/nix.conf
-nix profile install github:jeremyschlatter/nixpkgs
+nix profile install github:jeremyschlatter/env
 jeremy-post-install
