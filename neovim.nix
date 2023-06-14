b6e4d120e642e032e4cea6fccea8048e06357ea0
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2023-06-14 02:12:04 -0700

    drop local leap-nvim in favor of new upstream one

--- neovim.nix
+++ neovim.nix
@@ -1,16 +1,4 @@
 pkgs:
-let
-  leap = (pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "leap.nvim";
-    version = "2022-08-01";
-    src = pkgs.fetchFromGitHub {
-      owner = "ggandor";
-      repo = "leap.nvim";
-      rev = "5a09c30bf676d1392ff00eb9a41e0a1fc9b60a1b";
-      sha256 = "sha256-xmqb3s31J1UxifXauBzBo5EkhafBEnq2YUYKRXJLGB0=";
-    };
-  });
-in
   (pkgs.neovim.override {
     viAlias = true;
     vimAlias = true;
@@ -27,7 +15,7 @@ in
           ctrlp-vim
           fzf-vim
           fzfWrapper
-          leap
+          leap-nvim
           rust-vim
           vim-better-whitespace
           vim-commentary
