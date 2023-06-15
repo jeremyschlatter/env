03fd0b90ed081a09da8894d3f74bc8dfb6d46668
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2023-06-15 16:54:39 -0700

    scripts.nix: small rewrite

--- scripts.nix
+++ scripts.nix
@@ -47,24 +47,22 @@ crane: pkgs: src:
           preBuild = "rm *.go && cp ${f} ${name}.go && go mod edit -module ${name}";
         };
       };
-    inherit (lib) strings attrsets sources lists;
-  scripts = listToAttrs (filter (x: x != null) (lists.forEach (attrNames (readDir src)) (f:
-    let fullPath = src + "/${f}";
-    in if sources.pathIsDirectory fullPath then null else
-       let ext = elemAt (tail (split "\\." f)) 1;
-           name = strings.removeSuffix ".${ext}" f;
-           firstLine = head (split "\n" (strings.fileContents fullPath));
-           buildInfo = let x = strings.removeSuffix "#nix" firstLine; in
-             if x == firstLine then {} else fromJSON (elemAt (split "^[^ ]+" x) 2);
-           deps = map
-             (x: attrsets.getAttrFromPath (strings.splitString "." x)
-               (pkgs // { inherit scripts; }))
-             (buildInfo.deps or []);
-       in if hasAttr ext builders
-          then {
-            inherit name;
-            value = wrapPath name deps (getAttr ext builders buildInfo name fullPath);
-          }
-          else null
-    )));
+    scripts = listToAttrs (filter (x: x != null) (lib.attrsets.mapAttrsToList script (readDir src)));
+    script = f: kind:
+      let
+        inherit (lib) strings;
+        fullPath = src + "/${f}";
+        ext = elemAt (tail (split "\\." f)) 1;
+        name = strings.removeSuffix ".${ext}" f;
+        firstLine = head (split "\n" (strings.fileContents fullPath));
+        buildInfo = let x = strings.removeSuffix "#nix" firstLine; in
+          if x == firstLine then {} else fromJSON (elemAt (split "^[^ ]+" x) 2);
+        deps = map
+          (x: lib.attrsets.getAttrFromPath (strings.splitString "." x)
+            (pkgs // { inherit scripts; }))
+          (buildInfo.deps or []);
+      in if (kind == "regular" && hasAttr ext builders) then {
+        inherit name;
+        value = wrapPath name deps (getAttr ext builders buildInfo name fullPath);
+      } else null;
   in attrValues scripts
