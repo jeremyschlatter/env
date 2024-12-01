accf17d78e0bbb4130fdb66b9758a138bf7e160c
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2024-12-01 12:10:04 -0600

    nvim: configure treesitter incremental_selection

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,6 +2,15 @@ require'leap'.set_default_keymaps()
 
 require'nvim-treesitter.configs'.setup {
   highlight = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 }
 
 require'tabs-vs-spaces'.setup {
