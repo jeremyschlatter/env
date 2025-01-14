ce5bad2a2d1824dc1e4c04f049b101a970c1417d
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-01-13 20:46:25 -0800

    scripts/i.py: fail if update is a no-op
    `nix flake update --commit-lock-file` unfortunately returns
    _successfully_ even if it does not in fact update the lock file due to
    dependencies being unlocked. So check for that happening and fail if it
    does.
    
    I want to both stream `nix flake update`'s output to stdout and also
    programmatically read the output afterward, and the only succinct way I
    found to do that was using the sh package, so this commit also switches
    from subprocess to sh.

--- scripts/i.py
+++ scripts/i.py
@@ -1,12 +1,18 @@
+# {"deps": ["git"], "requirements": ["sh"]} #nix
 import json
 import os
 import sys
 
-from subprocess import check_call, check_output
+import sh
+
+
+def fail(s):
+    print(s, file=sys.stderr)
+    sys.exit(1)
 
 
 def read_metadata():
-    profile = check_output(['nix', 'profile', 'list', '--json']).decode()
+    profile = sh.nix('profile', 'list', '--json')
     targets = []
     for k, v in json.loads(profile)['elements'].items():
         for p in v['storePaths']:
@@ -15,22 +21,31 @@ def read_metadata():
                 break
 
     if len(targets) != 1:
-        print(
+        fail(
             'I need exactly one derivation with a "bundled-environment" ' +
             'store path to be in `nix profile list`',
-            file=sys.stderr,
         )
-        sys.exit(1)
 
     return targets[0]
 
 
 (package_name, flake_path) = read_metadata()
 
-check_call(
-    ['nix', 'flake', 'update', '--flake', flake_path, '--commit-lock-file'] +
-    ([] if os.getenv('I_DOT_PY_DO_FULL_UPDATE') else ['public-base'])
+output = sh.nix(
+    ['flake', 'update', '--flake', flake_path, '--commit-lock-file'] +
+    ([] if os.getenv('I_DOT_PY_DO_FULL_UPDATE') else ['public-base']),
+    _out=sys.stdout,
+    _tee=True,
 )
 
-check_call(['nix', 'profile', 'upgrade', package_name])
-check_call(['_jeremy-post-install'])
+if 'will not write lock file of flake' in output:
+    fail('check in your changes!')
+
+boop = {
+    '_in': sys.stdin,
+    '_out': sys.stdout,
+    '_err': sys.stderr,
+}
+
+sh.nix('profile', 'upgrade', package_name, **boop)
+sh.Command('_jeremy-post-install')(**boop)
