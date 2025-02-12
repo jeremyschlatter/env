59a76ea0a65bb66caade346236ffdc06b4164714
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-02-12 11:07:58 -0800

    crane: remove obsolete nixpkgs input

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = { url = github:NixOS/nixpkgs/nixpkgs-unstable; };
-    crane = { url = github:ipetkov/crane; inputs.nixpkgs.follows = "nixpkgs"; };
+    crane = { url = github:ipetkov/crane; };
     nixGL = { url = github:guibou/nixGL; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
