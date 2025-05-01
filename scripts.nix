2d83c5d9d1214b76e22a15fa6de970e60aa166f2
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-05-01 12:57:04 -0700

    simplify scripts.nix: inline craneLib

--- scripts.nix
+++ scripts.nix
@@ -20,14 +20,13 @@ crane: pkgs: src:
   with builtins;
   with pkgs;
   let
-    craneLib = crane.mkLib pkgs;
     builders =
       with writers;
       {
         sh = _: writeBashBin;
         py = { requirements ? [], ... }: name:
           writePython3Bin name { libraries = map (p: getAttr p python3Packages) requirements; };
-        rs = _: name: _: craneLib.buildPackage {
+        rs = _: name: _: (crane.mkLib pkgs).buildPackage {
           src = lib.sources.sourceFilesBySuffices src [".rs" "bin" ".toml" ".lock"];
           buildInputs = lib.optional stdenv.isDarwin libiconv;
           cargoExtraArgs = "--bin ${name}";
