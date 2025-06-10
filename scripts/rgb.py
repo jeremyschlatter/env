41def0e5b1531e46ce414ed67ec7007873f585f5
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-06-10 15:05:09 -0700

    rgb: allow passing arguments to rg

--- scripts/rgb.py
+++ scripts/rgb.py
@@ -1,8 +1,8 @@
 from subprocess import run
 from sys import argv, stderr
 
-if len(argv) != 2:
+if len(argv) < 2:
     print('usage: rgb <term>', file=stderr)
     exit(1)
 
-exit(run(['rg', fr'\b{argv[1]}\b']).returncode)
+exit(run(['rg', fr'\b{argv[1]}\b', *argv[2:]]).returncode)
