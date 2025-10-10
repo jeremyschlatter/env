8725df216e1e7e953c8a27051cc7cde06cf88815
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-10-09 23:19:10 -0700

    LS_COLORS: solarized light -> catppuccin mocha
    Also move it to a separate file because it's so long.

--- scripts.nix
+++ scripts.nix
@@ -26,8 +26,8 @@ crane: pkgs: src:
         sh = _: writeBashBin;
         py = { requirements ? [], ... }: name:
           writePython3Bin name { libraries = map (p: getAttr p python3Packages) requirements; };
-        rs = _: name: _: (crane.mkLib pkgs).buildPackage {
-          src = lib.sources.sourceFilesBySuffices src [".rs" "bin" ".toml" ".lock"];
+        rs = { include ? [], ...}: name: _: (crane.mkLib pkgs).buildPackage {
+          src = lib.sources.sourceFilesBySuffices src ([".rs" "bin" ".toml" ".lock"] ++ include);
           buildInputs = lib.optional stdenv.isDarwin libiconv;
           cargoExtraArgs = "--bin ${name}";
         };
