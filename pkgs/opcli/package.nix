0d8d90f38c0c7d6c3cdbb42b0b10e6bd3afdea9d
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2026-04-21 14:39:29 -0700

    opcli 0.13.0: TouchID window redesign (pre-release)

--- pkgs/opcli/package.nix
+++ pkgs/opcli/package.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "0.12.0";
+  version = "0.13.0";
   arch = if stdenv.hostPlatform.isAarch64 then "arm64" else "x86_64";
 in
 
@@ -15,7 +15,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/jeremyschlatter/opcli/releases/download/v${version}/opcli-v${version}-darwin-${arch}.tar.gz";
-    hash = "sha256-NeBqLg2H6e3VvjLu1FDCeTFmga4Zd9ZxnQP4s0AOCnU=";
+    hash = "sha256-MwJlWU/ySKDp1f7oMFHs6c1Lu6eFyN/nRZbONz0HCp0=";
   };
 
   sourceRoot = ".";
